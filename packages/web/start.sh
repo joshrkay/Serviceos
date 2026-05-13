1369835943415c413a392c7650208aed660728a6
Josh <60823931+joshrkay@users.noreply.github.com>
2026-05-13 12:28:11 -0700

    debug: log PORT and rendered nginx config in start.sh
    Add debug information and config validation to start.sh

--- packages/web/start.sh
+++ packages/web/start.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 set -e
 
+echo "=== DEBUG: PORT=${PORT} API_URL=${API_URL} ==="
+
 # Render nginx config from template
 envsubst '${PORT} ${API_URL}' \
   < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
+
+    echo "=== RENDERED NGINX CONFIG ==="
+    cat /etc/nginx/conf.d/default.conf
+    echo "=== END CONFIG ==="
+
+    # Validate nginx config
+    nginx -t
 
     # Render frontend runtime config
     envsubst '${VITE_CLERK_PUBLISHABLE_KEY} ${VITE_STRIPE_PUBLISHABLE_KEY}' \
